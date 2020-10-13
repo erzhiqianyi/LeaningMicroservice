b5989052795be031b95a51d1c6e0d41b13072235
erzhiqianyi <github@caofeng.me>
2020-10-13 17:26:51 +0800

    微信服务器校验配置

---
+++ wechat-server/src/main/java/top/erzhiqian/wechat/security/WeixinVerifyController.java
@@ -0,0 +1,17 @@
+package top.erzhiqian.wechat.security;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 微信消息推送配置和服务器配置,用于
+ * 具体参考微信文档
+ * <a href="https://developers.weixin.qq.com/miniprogram/dev/framework/server-ability/message-push.html">消息推送</a>
+ *
+ * 2020/8/24 10:30
+ * 曹峰
+ */
+@RestController
+@Log4j2
+public class WeixinVerifyController {
+}
